783ee28f3e93ede2038a06662332eaaa4a5582cc
imontalvomiguel <ubaldo.im@gmail.com>
2017-10-11 13:36:08 -0500

    fix white spaces

--- app.rb
+++ app.rb
@@ -3,13 +3,11 @@ require 'sinatra/activerecord'
 
 class Todo < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-
   set :environment, ENV['RACK_ENV']
 
   configure do
   end
 
   Dir[File.join(File.dirname(__FILE__), 'models', '*.rb')].each { |model| require model }
-
   Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each { |lib| load lib }
 end
