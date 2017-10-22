86a4d0a4c21cf085d0da7ed949acd48c100920ca
imontalvomiguel <ubaldo.im@gmail.com>
2017-10-22 16:21:47 -0500

    specs and code to create new users

--- app.rb
+++ app.rb
@@ -127,4 +127,23 @@ class App < Sinatra::Base
     redirect to('/tasks')
   end
 
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    @user = User.new
+
+    @user.first_name = params[:first_name]
+    @user.last_name = params[:last_name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if @user.save
+      redirect to('/lists')
+    else
+      halt erb(:error)
+    end
+  end
+
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -252,4 +252,40 @@ describe App do
     expect(last_response.body).not_to include('Build an image gallery in ruby')
   end
 
+  it "returns an HTML form for creating a new user" do
+    get "/users/new"
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('New user')
+  end
+
+  context "creating a new user" do
+    describe "with valid params" do
+      it "success" do
+        post "/users", {
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'johndoe@example.com',
+          password: '123456'
+        }
+
+        expect(User.all.size).to eq(1)
+        expect(User.first.first_name).to eq('John')
+        expect(User.first.last_name).to eq('Doe')
+        expect(User.first.email).to eq('johndoe@example.com')
+        expect(User.first.try(:authenticate, '123456')).to eq(User.first)
+      end
+    end
+
+    describe "with invalid params" do
+      it "fails" do
+        post "/users", {
+          email: 'email',
+          password: 'password'
+        }
+
+        expect(last_response.status).to eq(500)
+      end
+    end
+  end
 end
