4c1faa97a6f30ce493cf55ffc64adfca8832eb65
imontalvomiguel <ubaldo.im@gmail.com>
2017-10-28 02:07:02 -0500

    generate password_reset_token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,18 @@ describe User, type: :model do
     user = build(:user, email: 'EMAIL_at_EXAMPLE.COM', password: 'qwert')
     expect(user.save).to be_falsy
   end
+
+  describe "#generate_password_reset_token!" do
+    let(:user) { create(:user) }
+
+    it "changes the password_reset_token attribute" do
+      expect{ user.generate_password_reset_token! }.to change{ user.password_reset_token }
+    end
+
+    it "calls SecureRandom.urlsafe_base64 to generate the password_reset_totken" do
+      expect(SecureRandom).to receive(:urlsafe_base64)
+      user.generate_password_reset_token!
+    end
+  end
+
 end
